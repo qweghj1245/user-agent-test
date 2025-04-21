384854b2c3e8a175a52fcfeb156eba100b36f984
Mazusu <32471711+qweghj1245@users.noreply.github.com>
2025-04-21 09:50:43 +0800

    feat: init

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,66 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [results, setResults] = useState({
+    userAgent: "",
+    platform: "",
+    features: "",
+  });
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const detectByUserAgent = () => {
+      const ua = window.navigator.userAgent;
+      if (/iPhone|iPad|iPod/i.test(ua)) return "iOS";
+      if (/Android/i.test(ua)) return "Android";
+      return "Other";
+    };
+
+    const detectByPlatform = () => {
+      const platform = window.navigator.platform;
+      const maxTouchPoints = window.navigator.maxTouchPoints || 0;
+
+      if (
+        /iPhone|iPad|iPod/i.test(platform) ||
+        (platform === "MacIntel" && maxTouchPoints >= 1)
+      ) {
+        return "iOS";
+      }
+
+      if (/Linux|Android/.test(platform)) {
+        return "Android";
+      }
+
+      return "Other";
+    };
+
+    const detectByFeatures = () => {
+      if (window.webkit && window.webkit.messageHandlers) {
+        return "iOS";
+      }
+
+      if (
+        typeof window.navigator.share === "function" &&
+        window.navigator.canShare
+      ) {
+        return "Android";
+      }
+
+      return "Other";
+    };
+
+    setResults({
+      userAgent: detectByUserAgent(),
+      platform: detectByPlatform(),
+      features: detectByFeatures(),
+    });
+  }, []);
+
+  return (
+    <div>
+      <div>detect by user agent: {results.userAgent}</div>
+      <div>detect by user platform: {results.platform}</div>
+      <div>detect by user features: {results.features}</div>
     </div>
   );
 }
